c570a8828a39ee2c30e1d015ca29dd5b3a9afbfd
Noah Paiva <noahbpaiva@outlook.com>
2024-06-05 13:50:04 +0100

    refactor(app): move widgets

--- app/lib/common/presentation/presentation.dart
+++ app/lib/common/presentation/presentation.dart
@@ -1,1 +1,8 @@
-export 'widgets/widgets.dart';
+export 'widgets/app_bar/widget.dart';
+export 'widgets/blurred_container/widget.dart';
+export 'widgets/clickable_element/widget.dart';
+export 'widgets/icon_button/widget.dart';
+export 'widgets/loading/widget.dart';
+export 'widgets/navigation_bar/widget.dart';
+export 'widgets/novel_card/widget.dart';
+export 'widgets/separator/widget.dart';
